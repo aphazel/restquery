1e35ebdd05d9604fdc1a69bf1e7569b19016a6cd
janusz <john@evesaddiction.com>
2012-11-07 12:23:22 -0500

    Added some basic documentation.

--- src/MelonTech/RestQuery/RestQuery.php
+++ src/MelonTech/RestQuery/RestQuery.php
@@ -5,6 +5,7 @@ class RestQuery {
   const METHOD_POST = CURLOPT_POST;
   const METHOD_PUT = CURLOPT_PUT;
   const METHOD_GET = CURLOPT_HTTPGET;
+  const METHOD_DELETE = 128;
 
   protected $method = NULL;
   protected $post_data = NULL;
@@ -14,6 +15,11 @@ class RestQuery {
 
   protected $result = NULL;
 
+  /**
+   * Constructor. Optional $url argument, can be set later.
+   * @param string $url
+   * @throws RestQueryException
+   */
   public function __construct($url = NULL) {
     if(!function_exists('curl_init')) {
       throw new RestQueryException('CURL module not available!');
@@ -22,15 +28,33 @@ class RestQuery {
     if(!empty($url))
       $this->url = $url;
 
+    if(ini_get('open_basedir') == '' && strtolower(ini_get('safe_mode')) == 'off') {
+      $this->setCurlOpt(CURLOPT_FOLLOWLOCATION, TRUE);
+    }
+
     $this->curl_handle = curl_init($url);
     $this->setCurlOpt(CURLOPT_RETURNTRANSFER, TRUE);
   }
 
+  /**
+   * Set appropriate HTTPAuth options using RestQuery::setCurlOpt()
+   * @param string $user
+   * @param string $pass
+   * @param string $auth
+   */
   public function setAuth($user, $pass, $auth = 'basic') {
     $this->setCurlOpt(CURLOPT_HTTPAUTH, constant('CURLAUTH_'.strtoupper($auth)));
     $this->setCurlOpt(CURLOPT_USERPWD, "{$user}:{$pass}");
   }
 
+  /**
+   * Set appropriate proxy options using RestQuery::setCurlOpt()
+   * @param string $host
+   * @param string|int $port
+   * @param string $user Optional. HTTP Proxy user name
+   * @param string $pass Optional. HTTP
+   * @throws \InvalidArgumentException
+   */
   public function setProxy($host, $port, $user = NULL, $pass = NULL) {
     if(!is_numeric($port))
       throw new \InvalidArgumentException('Port must be numeric!');
@@ -43,17 +67,39 @@ class RestQuery {
       $this->setCurlOpt(CURLOPT_PROXYUSERPWD, "{$user}:{$pass}");
   }
 
+  /**
+   * Static function to validate the input URL. This can be overridden by
+   * subclasses to include custom validation. For instance, to validate
+   * certain host patterns for API-specific implementations. Returns boolean
+   * based on whether URL validates. Default behavior is to use filter_var()
+   * with FILTER_VALIDATE_URL set.
+   * @param string $url
+   * @return boolean
+   */
   public static function validate_url($url) {
     return (filter_var($url, FILTER_VALIDATE_URL)!==FALSE);
   }
 
+  /**
+   * Sets the data for a POST request.
+   * @param unknown $data
+   * @throws RestQueryException
+   * @return \MelonTech\RestQuery\RestQuery
+   */
   public function setPostData($data) {
     if($this->method != self::METHOD_POST)
       throw new RestQueryException('Method is not POST!');
-    $this->postdata = $post_data;
+    $this->post_data = $data;
+    $this->setCurlOpt(CURLOPT_POSTFIELDS, $data);
     return $this;
   }
 
+  /**
+   * Sets the URL for the request.
+   * @param string $url
+   * @throws RestQueryException
+   * @return \MelonTech\RestQuery\RestQuery
+   */
   public function setUrl($url) {
     if(!RestQuery::validate_url($url))
       throw new RestQueryException('Invalid URL!');
@@ -62,6 +108,13 @@ class RestQuery {
     return $this;
   }
 
+  /**
+   * curl_setopt() wrapper.
+   * @param int $option CURLOPT_* constant
+   * @param unknown $value Value as would be used in curl_setopt()
+   * @throws RestQueryException
+   * @return \MelonTech\RestQuery\RestQuery
+   */
   public function setCurlOpt($option, $value) {
     if(empty($this->curl_handle))
       throw new RestQueryException('No valid CURL handle!');
@@ -73,22 +126,50 @@ class RestQuery {
     return $this;
   }
 
+  /**
+   * Returns the currently-set HTTP method.
+   * @return Ambigous <NULL, unknown>
+   */
   public function getMethod() {
     return $this->method;
   }
+  /**
+   * Sets the HTTP method to use. Subclasses can include their own constants, but
+   * should obviously override this method.
+   * @param int $method Any of the RestQuery::METHOD_* constants.
+   * @throws RestQueryException
+   * @return \MelonTech\RestQuery\RestQuery
+   */
   public function setMethod($method) {
+    $this->method = $method;
     switch($method) {
-      case self::METHOD_GET:
+      case self::METHOD_DELETE:
+        $this->setCurlOpt(CURLOPT_CUSTOMREQUEST, 'DELETE');
+        break;
       case self::METHOD_POST:
+        $this->setCurlOpt(CURLOPT_POST, TRUE)
+          ->setCurlOpt(CURLOPT_CUSTOMREQUEST, 'POST');
+        break;
       case self::METHOD_PUT:
+        $this->setCurlOpt(CURLOPT_PUT, TRUE)
+          ->setCurlOpt(CURLOPT_CUSTOMREQUEST, 'PUT');
         $this->method = $method;
         break;
+      case self::METHOD_GET:
+        break;
       default:
+        $this->method = NULL;
         throw new RestQueryException('Invalid HTTP Method');
     }
     return $this;
   }
 
+  /**
+   * Set multiple CURL options.
+   * @param array $curlopts Array of CURLOPTs and their values. E.g., [CURLOPT_SSLVERSION, 3]
+   * @throws RestQueryException
+   * @return \MelonTech\RestQuery\RestQuery
+   */
   public function setCurlOpts(array $curlopts) {
     if(empty($this->curl_handle))
       throw new RestQueryException('No valid CURL handle!');
@@ -98,6 +179,11 @@ class RestQuery {
     return $this;
   }
 
+  /**
+   * Execute the query and return the raw string result.
+   * @throws RestQueryException
+   * @return mixed
+   */
   public function execute() {
     if(empty($this->curl_handle))
       throw new RestQueryException('No valid CURL handle!');
